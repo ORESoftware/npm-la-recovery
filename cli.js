bf616687a247465de66761a728fd69b87fbf2920
Olegzandr Denman <alex@oresoftware.com>
2018-05-29 21:52:15 -0700

    pdev:set

--- cli.js
+++ cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
 var path = require("path");
 var residence = require("residence");
 var lib_1 = require("./lib");
